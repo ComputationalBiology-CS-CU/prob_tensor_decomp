5c808f7e7a5f2bf2232f345a690f772dd3d4cb22
Rachel Ren <cr025@bucknell.edu>
2016-01-04 01:12:48 -0500

    Final COMMIT 1/4/2016

--- cplusplus/BayesianProbTensorDecomp/BayesianProbTensorDecomp/global_var.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  global_var.cpp
-//  BayesianProbTensorDecomp
-//
-//  Created by Rachel Ren on 12/27/15.
-//  Copyright © 2015 Rachel Ren. All rights reserved.
-//
-
-#include "global_var.hpp"
