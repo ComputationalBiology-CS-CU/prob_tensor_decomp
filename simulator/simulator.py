f66e8fc62c43bdfaa38d917fe0a422b11774e6c5
Rachel Ren <cr025@bucknell.edu>
2016-02-26 17:21:20 -0500

    add simulator.py

--- simulator/simulator.py
+++ simulator/simulator.py
@@ -8,6 +8,7 @@ from scipy.stats import bernoulli
 import math
 from numpy import linalg as LA
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 ##=====================
 ##==== global variables
@@ -15,10 +16,13 @@ import matplotlib.pyplot as plt
 n_factor = 0
 n_individual = 0
 n_gene = 0
+n_tissue = 0
 #The following parameters need to be determined by test-and-trials
+#According to Barbara, they used alpha=beta=1 for the uniform on sparsity
+#alpha = 1 beta = 2 is a line of y = -2x + 2
 beta = [1,2]                #parameters of beta[alpha, beta]
 normalGamma = [1,2,1,2]     #parameters of NG[mu, kappa, alpha, beta]
-normalWishart = [[2,2],2,[[1,0],[0,1]],1]   #parameters of NW[mu, kappa, Lambda, v]
+normalWishart = [[2,2],2,[[10,5],[5,10]],3]   #parameters of NW[mu, kappa, Lambda, v]
 
 ##=================================================
 ##===The Following code is adapted from Shuo's code
@@ -45,10 +49,22 @@ def get_individual_id(s):
 
 
 ##==== load and format the target dataset
-## TODO: the current dir are all in my local Mac (Shuo)
+## TODO: the current dir are all in my local Mac
 def data_prepare():
     global n_individual
     global n_gene
+    global n_tissue
+
+    #== get the number of tissue types
+    file = open("/Users/rachelren/Documents/Columbia_CS_Research/expression_by_etissue/tissue_list.txt", 'r')
+    count = 0
+    while 1:
+        line = file.readline()
+        if not line:
+            break
+        count += 1
+    file.close()
+    n_tissue = count
 
     # == get all the individuals
     individual_rep = {}  # map individual ID into its index in the tensor
@@ -56,7 +72,7 @@ def data_prepare():
     for i in range(n_tissue):
         index_tissue = i + 1
         file = open(
-            "/Users/shuoyang/Desktop/Genetics_GeneExpression/GTEx/workbench_v.6/data_processed/expression_by_etissue/tissue_" + str(
+            "/Users/rachelren/Documents/Columbia_CS_Research/expression_by_etissue/tissue_" + str(
                 index_tissue) + ".txt", 'r')
         line = (file.readline()).strip()
         line = line.split('\t')[1:]
@@ -70,7 +86,7 @@ def data_prepare():
 
     # == get the number of genes
     file = open(
-        "/Users/shuoyang/Desktop/Genetics_GeneExpression/GTEx/workbench_v.6/data_processed/expression_by_etissue/tissue_1.txt",
+        "/Users/rachelren/Documents/Columbia_CS_Research/expression_by_etissue/tissue_1.txt",
         'r')
     file.readline()
     count = 0
@@ -106,8 +122,8 @@ def sampler_MVN(mean, cov):
 def sampler_W(df, scale):
     #
     sample = wishart.rvs(df, scale, size=1, random_state=None)
-    matrix = sample[0]
-    return matrix
+#    matrix = sample[0]
+    return sample
 
 
 ##==== sampling from Gamma
@@ -157,11 +173,11 @@ def simulator_MVN(n_sample, isTrans):
     u = []
 
     for sample in range(n_sample):
-        precisionMatrix = sampler_W(normalWishart[2], normalWishart[3])
+        precisionMatrix = sampler_W(normalWishart[3], normalWishart[2])
         precisionMatrix_scaled = []
-        for i in range(precisionMatrix):
+        for i in range(len(precisionMatrix)):
             temp = []
-            for j in range(precisionMatrix[0]):
+            for j in range(len(precisionMatrix[0])):
                 temp.append(precisionMatrix[i][j]/normalWishart[1])
             precisionMatrix_scaled.append(temp)
         mu = np.random.multivariate_normal(normalWishart[0], precisionMatrix_scaled)
@@ -171,6 +187,30 @@ def simulator_MVN(n_sample, isTrans):
         return np.array(u).T
     else:
         return u
+
+def compare_sparsity (v1, v2):
+    k1 = len(n_gene)
+    k2 = len(n_gene)
+
+    sigma = []   #correlation matrix K1-by-K2
+    for i in range(n_factor):
+        sigma_row = []
+        for j in range(n_factor):
+            sigma_row.append(calc_corr(v1[i], v2[j]))
+        sigma.append(sigma_row)
+
+    sum_col = 0
+    for row in range(len(sigma)):
+        for col in range(len(sigma[row])):
+            #TODO!
+            continue
+
+    sum_col = 0
+
+def calc_corr (col1, col2):
+    #TODO!
+    return
+
 
 if __name__ == '__main__':
 
@@ -222,26 +262,74 @@ if __name__ == '__main__':
 
     normalWishart[0] = mu
     normalWishart[2] = precision
+    normalWishart[3] = n_factor + 1
+
+    #DEBUG
+    print "finish initializing all the variables..."
+
+    #DEBUG
+    print "now start simulation..."
 
     if (useSpike):
+        #DEBUG
+        print "Start Spike and Slab simulation..."
+
         v = simulator_spike_slab()
+
+        #DEBUG
+        print "finish Spike and Slab simulation..."
+
+        #DEBUG
+        print "Start MVN simulation..."
+
         u = simulator_MVN(n_individual, False)
+
+        #DEBUG
+        print "finish MVN simulation..."
 
     else:
         v = simulator_MVN(n_gene, True)
         u = simulator_MVN(n_individual, False)
 
+    '''
 
+    #DEBUG
+    print "matrix multiplication..."
 
+    product = np.dot(u, v).T
 
+    #DEBUG
+    print "prepare to draw matrix..."
+    
+    sns.set(context="paper", font="monospace")
+    # Set up the matplotlib figure
+    f, ax = plt.subplots(figsize=(12, 9))
 
+    
+    #DEBUG
+    print "draw product using seaborn..."
+    
+    # Draw the heatmap using seaborn
+#    sns_plot = sns.heatmap(product)
+    sns_plot = sns.heatmap(v)
+    
+#    f.tight_layout()
+    
+    #DEBUG
+    print "finish drawing product..."
+    
+    #DEBUG
+    print "saving figures..."
+    
+    fig = sns_plot.get_figure()
+    fig.savefig("sparsity.png")
 
-
-
-
-
-
-
-
+    
+    
+    
+    #DEBUG
+    print "End of program..."
+    '''
+    
 
 
