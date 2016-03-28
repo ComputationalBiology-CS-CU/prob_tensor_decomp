d744c1de8d590f49be27caf2f5c6c315aa6358c5
renchuqiao <cr025@bucknell.edu>
2016-03-28 15:58:03 -0400

    add test

---
+++ test/sampler_factor_test.py
@@ -0,0 +1,11 @@
+#This is the test code for sampler_factor
+
+
+def test_cr(v, t, alpha, lambda_u):
+	print "hello world"
+
+def test_mw(v,t,alpha, lambda_u):
+	print "hello mengqing"
+
+def test_main():
+	print "The main function"
