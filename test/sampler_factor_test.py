d66b416d1ae80890ade7653dd27e9c6e203bd42c
mengqwang2 <mengqwang2@gmail.com>
2016-03-28 16:39:23 -0400

    sum up precision matrices

--- test/sampler_factor_test.py
+++ test/sampler_factor_test.py
@@ -4,8 +4,17 @@
 def test_cr(v, t, alpha, lambda_u):
 	print "hello world"
 
-def test_mw(v,t,alpha, lambda_u):
-	print "hello mengqing"
+
+def test_mw(v, t, alpha, lambda_u):
+	#print "hello mengqing"
+    d1 = v.shape
+    d2 = t.shape
+    for i in range(d1[0]):
+        for j in range(d2[0]):
+            vt_vector = np.multiply(v[i], t[j])
+            precision_matrix = np.add(precision_matrix, alpha * np.dot(np.array([vt_vector]).T, np.array([vt_vector])))
+    return precision_matrix
+
 
 def test_main():
 	print "The main function"
