b9ad5c99a9d6564c7a095dd6eb44bfda5c305b0b
renchuqiao <cr025@bucknell.edu>
2016-03-31 17:34:24 -0400

    Modify sampler_test file again

--- test/sampler_factor_test.py
+++ test/sampler_factor_test.py
@@ -1,27 +1,31 @@
 #This is the test code for sampler_factor
 import numpy as np
 from scipy.stats import wishart
+import timeit
 
 #====================
 #==Global variables==
 #====================
-n_factor = 30
-n_v = 20000
-n_t = 200
+n_factor = 10
+n_v = 200
+n_t = 20
+alpha = 0.5
 normalWishart = [[2,2],2,[[10,5],[5,10]],3]
 
-def test_cr(v, t, alpha, lambda_u, precision_matrix):
+def test_cr(v, t, lambda_u, precision_matrix):
+	global alpha
 	print "hello world"
 
 
-def test_mw(v, t, alpha, lambda_u, precision_matrix):
-	d1 = v.shape
-	d2 = t.shape
+def test_mw(v, t,  lambda_u, precision_matrix):
+	global alpha
+	d1 = n_v
+	d2 = n_t
 
-	for i in range(d1[0]):
-		for j in range(d2[0]):
+	for i in range(d1):
+		for j in range(d2):
 			vt_vector = np.multiply(v[i], t[j])
-			precision_matrix = np.add(precision_matrix, alpha * np.dot(np.array([vt_vector]).T, np.array([vt_vector])))
+			precision_matrix = np.add(precision_matrix, alpha * np.dot(np.array(vt_vector).T, np.array(vt_vector)))
 	return precision_matrix
 
 
@@ -33,19 +37,45 @@ def test_main():
 
 
 	#DEBUG
-	print "Initiate precision_matrix"
+	print "Initiate precision_matrix and lambda_U"
 	precision = []		# will be initialized with a diagonal 1 matrix
+	lambda_U = []
 	for i in range(n_factor):
 		precision.append([])
+		lambda_U.append([])
 		for j in range(n_factor):
 			if j == i:
 				precision[i].append(1)
+				lambda_U[i].append(1)
 			else:
 				precision[i].append(0)
+				lambda_U[i].append(0)
 	precision = np.array(precision)
+	lambda_U = np.array(lambda_U)
 
-	simulator_MVN()
 
+	#DEBUG
+	print "simulate V and T"
+	v = simulator_MVN(n_v, False)
+	t = simulator_MVN(n_t, False)
+
+	#DEBUG
+	print "test Mengqing's method"
+	start_time_mw = timeit.default_timer()
+	test_mw(v, t, lambda_U, precision)
+	elapsed_mw = timeit.default_timer() - start_time_mw
+
+	print "test Chuqiao's method"
+	start_time_cr = timeit.default_timer()
+	test_cr(v, t, lambda_U,precision)
+	elapsed_cr = timeit.default_timer() - start_time_cr
+
+
+	print "---------RESULTS SECTION--------------"
+
+	print("Mengqing's result is ", elapsed_mw)
+	print "---------------------------------"
+	print("Chuqiao's result is ", elapsed_cr)
 
 
 #========================
@@ -71,10 +101,13 @@ def simulator_MVN(n_sample, isTrans):
 	if (isTrans):
 		return np.array(u).T
 	else:
-		return u
+		return np.array(u)
 
 def sampler_W(df, scale):
 	#
     sample = wishart.rvs(df, scale, size=1, random_state=None)
 #    matrix = sample[0]
     return sample
+
+if __name__ == '__main__':
+	test_main()
